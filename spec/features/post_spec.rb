b4f38a1037afe521e077139c1505c3388a831dec
ttran9 <toddtran9@gmail.com>
2017-12-04 16:43:26 -0800

    Refactoring the index page tests to log in first before visitng the posts webpage

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,13 +2,18 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
+      user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf',
+                         first_name: 'Jon', last_name: 'Snow')
+      login_as(user, :scope => :user)
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
   end
