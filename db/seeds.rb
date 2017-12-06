5f36c2ded8ddc5df50a667dc59770feab05408f2
ttran9 <toddtran9@gmail.com>
2017-12-06 12:34:52 -0800

    added generating an AdminUser

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-User.create(email: 'admin@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf',
+User.create(email: 'user@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf',
                    first_name: 'Jon', last_name: 'Snow')
 
 puts "User created"
+
+AdminUser.create(email: 'admin@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf',
+            first_name: 'Todd', last_name: 'Tran')
+
+puts "Admin User created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: User.last.id)
