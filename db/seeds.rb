e238a59140e3234d4b82cf9ed042c27f547ab41f
ttran9 <toddtran9@gmail.com>
2017-12-07 14:26:25 -0800

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-User.create(email: 'user@test.com',
+@user = User.create(email: 'user@test.com',
             password: 'asdfasdf',
             password_confirmation: 'asdfasdf',
             first_name: 'Jon',
             last_name: 'Snow',
-            phone: '"4849862197')
+            phone: '4849862197')
 
 puts "User created"
 
@@ -23,7 +23,7 @@ end
 puts "100 posts have been created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
