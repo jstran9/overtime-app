25874a782d0f80bfd873e9b61defdc6e5a79365f
ttran9 <toddtran9@gmail.com>
2017-12-07 14:19:02 -0800

    setting start date to be 6 days before current date

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :audit_log do
     user
     status 0
-    start_date (Date.today.beginning_of_week(:saturday))
+    start_date (Date.today - 6.days)
     end_date nil
   end
 end
