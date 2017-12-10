166ce1a2a2ddffeaa25c21a085225c41671f23c9
ttran9 <toddtran9@gmail.com>
2017-12-09 19:55:07 -0800

    Updated production asset management rules

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.serve_static_assets = true
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
 end
