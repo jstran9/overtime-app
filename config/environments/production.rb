df0d5275e856247a786e84bc3ad098586362f0af
ttran9 <toddtran9@gmail.com>
2017-12-09 23:13:58 -0800

    fixing production email sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,4 +24,17 @@ Rails.application.configure do
   config.assets.compile = true
 
   config.action_mailer.default_url_options = { :host => 'tt-overtime.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'tt-overtime.herokuapp.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
